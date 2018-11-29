d2e809a68dca77ed21992597a6979bc5cc277fab
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-11-29 22:16:19 +0700

    More debugging logs

--- companion.php
+++ companion.php
@@ -10,6 +10,12 @@ Author: Osk
 error_log( print_r( 'COMPNION PLUGIN', 1 ) );
 error_log( print_r( $_GET, 1 ) );
 
+if ( ( function_exists('is_gutenberg_page') && is_gutenberg_page() ) || ( function_exists('has_blocks') && has_blocks( $content ) )  ) {
+		// DO SOMETHING
+		error_log( print_r( 'IS GUTENBERG!', 1 ) );
+		error_log( print_r( $_GET, 1 ) );
+}
+
 if ( is_multisite() && ! is_main_site() ) {
 	add_action( 'pre_current_active_plugins', 'companion_hide_plugin' );
 	add_action( 'admin_notices', 'companion_admin_notices' );
